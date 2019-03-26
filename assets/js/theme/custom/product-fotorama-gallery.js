7c9255c019a2ac0d0931d8672249b9a45c720b33
Silifonov Alexey <silifonov@gmail.com>
2019-03-26 17:16:08 +0200

    start editing PDP

---
+++ assets/js/theme/custom/product-fotorama-gallery.js
@@ -0,0 +1,11 @@
+/*
+ Import all product specific js
+ */
+import PageManager from '../page-manager';
+
+export default class ProductFotoramaGallery extends PageManager {
+
+    onReady() {
+        console.log("On products page message from js");
+    }
+}

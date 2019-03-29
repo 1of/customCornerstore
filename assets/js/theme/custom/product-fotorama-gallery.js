5e22a13894fdb9543e4542839712d755ba1e7657
Silifonov Alexey <silifonov@gmail.com>
2019-03-29 18:02:36 +0200

    add fotorama among slick stencil gallery

--- assets/js/theme/custom/product-fotorama-gallery.js
+++ assets/js/theme/custom/product-fotorama-gallery.js
@@ -2,10 +2,19 @@
  Import all product specific js
  */
 import PageManager from '../page-manager';
+import $ from 'jquery';
+import 'fotorama/fotorama.js';
 
 export default class ProductFotoramaGallery extends PageManager {
 
     onReady() {
-        console.log("On products page message from js");
+        const $fotoramaDiv = $('#fotorama').fotorama();
+        const fotorama = $fotoramaDiv.data('fotorama');
+
+
+        $('.fotorama__stage__shaft').on('click', (event) => {
+            event.preventDefault();
+            fotorama.requestFullScreen();
+        });
     }
 }

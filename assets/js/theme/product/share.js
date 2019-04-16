dc1010acf425c2c46942f1395402a9fe896bf3a6
Alexey <silifonov@gmail.com>
2019-04-16 23:42:30 +0300

    share button add image

--- assets/js/theme/product/share.js
+++ assets/js/theme/product/share.js
@@ -3,8 +3,8 @@ import $ from 'jquery';
 export default function () {
   /* eslint-disable */
     window.onload = function() {
-        $('.at-share-btn-elements').css({'opacity': '0'});
-        $('.at-share-bt').css({'background-color': 'transparent'});
-        $('.at-icon-addthis').css({'fill': 'transparent'});
+        //$('.at-share-btn-elements').css({'opacity': '0'});
+        //$('.at-share-bt').css({'background-color': 'transparent'});
+        //$('.at-icon-addthis').css({'fill': 'transparent'});
     };
 }
